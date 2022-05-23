bcf62b24bcc66d1bce8ff5c4987561eb0ad196f1
DEN <78467727+denaldTrr@users.noreply.github.com>
2022-05-23 23:35:28 +0300

    init: add solutions

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+
+  fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+    if (err) throw err;
+
+    files.forEach((file) => {
+      fs.unlink(path.join(__dirname, 'files-copy', file), () => {});
+    });
+  });
+});
+
+fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+  if (err) throw err;
+
+  files.forEach((file) => {
+    fs.writeFile(path.join(__dirname, 'files-copy', file), '', {}, (err) => {
+      if (err) throw err;
+
+      fs.copyFile(
+        path.join(__dirname, 'files', file),
+        path.join(__dirname, 'files-copy', file),
+        () => {}
+      );
+    });
+  });
+});
